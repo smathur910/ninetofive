8dfd59840e466ed1e46272a7b17a9b570b79ee0a
smathur910 <smathur910@gmail.com>
2023-08-13 12:21:57 +0530

    homepage two section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeroBannerComponent } from './components/shared/hero-banner/hero-banner.component';
+import { ServiceBlocksComponent } from './components/shared/service-blocks/service-blocks.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroBannerComponent,
+    ServiceBlocksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/components/shared/service-blocks/service-blocks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-blocks',
+  templateUrl: './service-blocks.component.html',
+  styleUrls: ['./service-blocks.component.css']
+})
+export class ServiceBlocksComponent {
+
+}
